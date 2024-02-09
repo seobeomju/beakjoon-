2ca753cced2d03e13d059b925f3140e3754e007a
seobeomju <alt10063@naver.com>
2024-02-09 17:44:46 +0900

    240209

--- 2/2-7.py
+++ 2/2-7.py
@@ -1,6 +1,6 @@
 a,b,c=map(int,input().split())
 if a==b==c:
-    print(1000+a*1000)
+    print(10000+a*1000)
 elif a==b:
     print(1000+a*100)
 elif b==c:
@@ -9,4 +9,3 @@ elif a==c:
     print(1000+c*100)
 else:
     print(max(a,b,c)*100)
-    
